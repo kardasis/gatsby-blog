f15a3cc348a4c75cc2618ba9794f20d6cd3f29d8
Ari Kardasis <ari.kardasis@gmail.com>
2022-09-20 14:46:19 -0400

    remove netlify cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,3 @@
-import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
-
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
